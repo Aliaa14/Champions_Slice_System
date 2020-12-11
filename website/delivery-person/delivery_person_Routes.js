68e5ef5525cc755de0ef9fc6540988a7ef5453a9
aliaa14 <aabdelr001@citymail.cuny.edu>
2020-12-10 20:39:32 -0500

    delivery person 1st edition

---
+++ website/delivery-person/delivery_person_Routes.js
@@ -0,0 +1,7 @@
+import React, { Component, component } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import delivery_person from './delivery_person/derivery_person.js'
+
+class delivery_person_Routes extends Component {
+
+}
